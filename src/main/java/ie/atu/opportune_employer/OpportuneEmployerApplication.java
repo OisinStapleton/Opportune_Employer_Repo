532572f045874411971f5424df689cd04edd0511
OisinStapleton <G00365445@atu.ie>
2026-04-02 16:43:11 +0100

    First commit for employer microservice

---
+++ src/main/java/ie/atu/opportune_employer/OpportuneEmployerApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.opportune_employer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpportuneEmployerApplication {
+    // main application - server port 8082
+    public static void main(String[] args) {
+        SpringApplication.run(OpportuneEmployerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/opportune_employer/OpportuneEmployerApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.opportune_employer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpportuneEmployerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
